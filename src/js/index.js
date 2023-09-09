ee37683967f83bb0bbfd8c5d9de975e441e4d2ec
Diana Nazarova <alllleksandrova@mail.ru>
2023-09-09 16:22:41 +0300

    feat: add swiper

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,15 @@ autoCountryComplete();
 
 import datePicker from "./modules/date-picker";
 datePicker();
+
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+	slidesPerView: 4,
+	spaceBetween: 32,
+	navigation: {
+		nextEl: '#sliderNext',
+		prevEl: '#sliderPrev',
+	},
+});
